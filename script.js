8bb3fa6ada50a7e46691302fa0ea79846c8b6c11
Tonny2010 <r.luiz21@escola.pr.gov.br>
2026-05-27 11:02:20 -0300

    a

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 const player = document.getElementById("player");
+const platforms = document.querySelectorAll(".platform");
 
-let positionX = 100;
+let positionX = 40;
+let positionY = 120;
+
 let velocityY = 0;
 let isJumping = false;
 
+const gravity = 1;
 const groundHeight = 120;
-let positionY = groundHeight;
 
-// Movimento lateral
+// Movimento
 document.addEventListener("keydown", (event) => {
 
   if(event.code === "ArrowRight"){
-    positionX += 40;
+    positionX += 20;
   }
 
   if(event.code === "ArrowLeft"){
-    positionX -= 40;
+    positionX -= 20;
   }
 
-  // Pulo
+  // Pulo na seta pra cima
   if(event.code === "ArrowUp" && !isJumping){
     velocityY = 18;
     isJumping = true;
@@ -27,20 +30,58 @@ document.addEventListener("keydown", (event) => {
   player.style.left = positionX + "px";
 });
 
-// Gravidade
+// Game Loop
 function gameLoop(){
 
-  velocityY -= 1;
-
+  velocityY -= gravity;
   positionY += velocityY;
 
-  // Limite do chão
+  let onPlatform = false;
+
+  // Colisão com plataformas
+  platforms.forEach(platform => {
+
+    const platformLeft = platform.offsetLeft;
+    const platformRight = platformLeft + platform.offsetWidth;
+
+    const platformBottom = platform.offsetTop + platform.offsetHeight;
+    const platformTop = platform.offsetTop;
+
+    const playerLeft = positionX;
+    const playerRight = positionX + 80;
+
+    const playerBottom = window.innerHeight - positionY;
+    const playerTop = playerBottom - 80;
+
+    // Detecta pouso
+    if(
+      playerRight > platformLeft &&
+      playerLeft < platformRight &&
+      playerBottom <= platformTop + 20 &&
+      playerBottom >= platformTop - 20 &&
+      velocityY <= 0
+    ){
+      positionY = window.innerHeight - platformTop;
+      velocityY = 0;
+      isJumping = false;
+      onPlatform = true;
+    }
+
+  });
+
+  // Colisão com chão
   if(positionY <= groundHeight){
     positionY = groundHeight;
     velocityY = 0;
     isJumping = false;
   }
 
+  // Se não estiver em plataforma nem chão
+  if(positionY > groundHeight && !onPlatform){
+    isJumping = true;
+  }
+
+  player.style.left = positionX + "px";
   player.style.bottom = positionY + "px";
 
   requestAnimationFrame(gameLoop);
